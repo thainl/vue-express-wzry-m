06381c794b7efd025f4b77c35ab0dc496613736a
thainl <thainl@qq.com>
2021-01-13 22:58:21 +0800

    fix errors when token is invalid

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,9 +5,13 @@ module.exports = options => {
     return async (req, res, next) => {
         const token = String(req.headers.authorization || '').split(' ').pop(); // 获取数组最后一个
         assert(token, 401, 'token不存在，请先登录'); // 抛出错误，在最后统一处理
-        const { id } = jwt.verify(token, req.app.get('secret')); // 校验解密token
-        assert(id, 401, '无效的token，请重新登录');
-        req.user = await AdminUser.findById(id).lean();
+        let tokenObj;
+        try {
+            tokenObj = jwt.verify(token, req.app.get('secret')); // 校验解密token
+        } catch (err) {
+            assert(false, 401, '无效的token，请重新登录');
+        }
+        req.user = await AdminUser.findById(tokenObj.id).lean();
         assert(req.user, 401, '请登录');
         next();
     }
