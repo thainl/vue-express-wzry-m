b4fa9f30d7ab8381137f80212bedbb41d8108ffe
thainl <thainl@qq.com>
2021-01-18 12:03:24 +0800

    feat(admin): 把用到的api接口提取到单独的文件

---
+++ admin/src/plugins/api.js
@@ -0,0 +1,51 @@
+import http from "./http";
+
+async function createResourceItem(model, data) {
+    return await http.post("/rest/" + model, data);
+}
+
+async function getResourceList(model, page, size, sort, search) {
+    // 排序搜索为可选
+    return await http.get(
+        `/rest/${model}?page=${page}&size=${size}${
+            (sort ? "&sort=" + sort : "") + (search ? "&search=" + search : "")
+        }`
+    );
+}
+
+async function deleteResourceItem(model, id) {
+    if (!id) throw new Error("要删除的id不能为空");
+    return await http.delete("/rest/" + model + "/" + id);
+}
+
+async function deleteResources(model, arr) {
+    if (!Array.isArray(arr)) return;
+    return await http.delete("/rest/" + model, { data: arr });
+}
+
+async function getResourceItem(model, id) {
+    return await http.get("/rest/" + model + "/" + id);
+}
+
+async function updateResourceItem(model, id, data) {
+    return await http.put("/rest/" + model + "/" + id, data);
+}
+
+async function adminUserLogin(data) {
+    return await http.post('/login', data);
+}
+
+async function getLoginUserInfo() {
+    return await http.get('/user_info');
+}
+
+export {
+    createResourceItem,
+    getResourceList,
+    deleteResourceItem,
+    deleteResources,
+    getResourceItem,
+    updateResourceItem,
+    adminUserLogin,
+    getLoginUserInfo,
+};
